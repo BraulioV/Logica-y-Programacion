ddbd6f1ac8aec91bd8f33bdc6c38a440a6bea24f
mgmacias95 <mgmacias95@correo.ugr.es>
2016-01-19 16:45:08 +0100

    completo_cifra_afin

--- Ejercicio1/xeuclides.hs
+++ Ejercicio1/xeuclides.hs
@@ -28,13 +28,27 @@ xeuclidesAux a b = [d, m, n - (a `div` b) * m]
             [d,n,m] = xeuclidesAux b (a `mod` b)
 
 -- Algoritmo extendido de euclides con una red de procesos
-xeuclides_red :: Int -> Int -> Int -> Int -> Int -> Int -> [[Int]]
-xeuclides_red a b u0 u1 v0 v1 = takeWhile ((a `mod` b) > 0) [q,b,m,u0',u1',v0',v1']:xeuclides_red b m u0' u1' v0' v1'
+xeuclides_red :: Int -> Int -> [(Int,Int,Int,Int,Int,Int,Int)]
+xeuclides_red a b = (0,a,b,1,0,0,1):map e (xeuclides_red a b)
+    where 
+        e (c,a,b,u0,u1,v0,v1) = (q,b,r,u1,u1',v1,v1')
+            where
+                (q,r) = a `divMod` b
+                u1'   = u0 - (q * u1)
+                v1'   = v0 - (q * v1)
+
+-- funcion para evitar que se produzca la excepcion de dividir por cero
+xeuclides_redTW :: Int -> Int -> [(Int,Int,Int,Int,Int,Int,Int)]
+xeuclides_redTW a b = takeWhile cd (xeuclides_red a b)
     where
-        q   = a `div` b
-        m   = a `mod` b
-        u0' = u1
-        u1' = u0 - (q * u1)
-        v0' = v1
-        v1' = v0 - (q * v1) 
--- dice olmedo que esto esta mal
+        cd :: (Int,Int,Int,Int,Int,Int,Int) -> Bool
+        cd (q,a,b,u0,u1,v0,v1)
+            | b == 0    = False
+            | otherwise = True
+
+xeuclides_redIT :: Int -> Int -> (Int,Int,Int)
+xeuclides_redIT 0 b = (b * signum b, 0, signum b)
+xeuclides_redIT a 0 = (a * signum a, signum a, 0)
+xeuclides_redIT a b = (d * signum d, signum d * u0, signum d * v0)
+    where
+        (q,a1,d,u0,u1,v0,v1) = last (xeuclides_redTW a b)
