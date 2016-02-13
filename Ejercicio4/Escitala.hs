e2b183258568272908b83045d4249b18dadd4411
mgmacias95 <mgmacias95@correo.ugr.es>
2016-02-14 00:58:19 +0100

    escitalals

--- Ejercicio4/Escitala.hs
+++ Ejercicio4/Escitala.hs
@@ -1,2 +1,27 @@
 module Escitala (module Escitala) where 
     
+    import Data.Matrix
+    import Data.Vector
+    import Data.List
+
+    cifrar :: String -> Int -> String
+    cifrar texto n
+        | (Prelude.length texto) `mod` n == 0 = cifrarAux (Data.Matrix.fromList a n texto) n ""
+        | otherwise = cifrarAux (Data.Matrix.fromList (a+1) n (addWhiteSpaces texto b)) n ""
+            where
+                a = (Prelude.length texto) `div` n
+                b = (n*(a+1)) - (Prelude.length texto)
+
+    descifrar :: String -> Int -> String
+    descifrar texto n = dropWhileEnd (== ' ') (cifrarAux (Data.Matrix.fromList n a texto) a "")
+        where
+            a = (Prelude.length texto) `div` n
+            b = (n*(a+1)) - (Prelude.length texto)
+
+    addWhiteSpaces :: String -> Int -> String
+    addWhiteSpaces texto 0 = texto
+    addWhiteSpaces texto n = addWhiteSpaces (texto Prelude.++ " ") (n-1)
+
+    cifrarAux :: Matrix Char -> Int -> String -> String
+    cifrarAux _ 0 r = r
+    cifrarAux m n r = cifrarAux m (n-1) ((Data.Vector.toList (getCol n m)) Prelude.++ r)
