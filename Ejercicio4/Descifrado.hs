aa043a89fce05e1f20a30c9dc7bb6ad5cc9beb10
mgmacias95 <mgmacias95@correo.ugr.es>
2016-02-16 00:22:35 +0100

    descifrado

--- Ejercicio4/Descifrado.hs
+++ Ejercicio4/Descifrado.hs
@@ -1,27 +1,70 @@
-module Descifrado (module Descifrado) where
+module Descifrado (descifrar) where
 
     import Data.Matrix      -- install with cabal install Matrix
     import Data.Vector
 
-    noRepeats :: (Eq a, Ord a) => [a] -> [a]
-    noRepeats [] = []
-    noRepeats [x] = [x]
-    noRepeats (x:xs) = x:(filter (/= x) (noRepeats xs))
+    eliminarEspacios :: String -> String
+    eliminarEspacios texto = Prelude.filter (/= ' ') texto
 
-    qsort_e :: (Eq a, Ord a) => [a] -> [a]
-    qsort_e [] = []
-    qsort_e (x:xs) = qsort_e (filter (< x) xs) ++ [x] ++ qsort_e(filter(>= x) xs)
+    unirStrings :: [String] -> String
+    unirStrings xs = Prelude.concat xs
 
+    partirMasResto :: String -> Int -> Matrix Char
+    partirMasResto texto n = transpose (Data.Matrix.fromList n a texto)
+        where
+            a = (Prelude.length texto) `div` n
 
-    sortSr :: (Eq a, Ord a) => [a] -> [a]
-    sortSr xs = noRepeats (qsort_e xs)
+    toLista :: Matrix Char -> [[Char]]
+    toLista m = toListaAux m (ncols m) []
+        where 
+            toListaAux :: Matrix Char -> Int -> [[Char]] -> [[Char]]
+            toListaAux _ 0 l = l
+            toListaAux m n l = toListaAux m (n-1) (ml:l)
+                where
+                    ml = Data.Vector.toList (getCol n m)
 
-   partirMasResto :: String -> Int -> Matrix Char
-    partirMasResto texto n
-        | (Prelude.length texto) < n = error "longitud del mensaje menor a la clave"
-        | b == 0             = Data.Matrix.fromList a n texto
-        | otherwise          = Data.Matrix.fromList (a+1) n t
-            where
-                a = (Prelude.length texto) `div` n
-                b = (Prelude.length texto) `mod` n
-                t = addWhiteSpaces texto (n - b)
+    columnasConEspacios :: [[Char]] -> [Bool]
+    columnasConEspacios xs = Prelude.map (\x -> Prelude.last x == ' ') xs
+
+    transponerLista :: Matrix Char -> [Int] -> String
+    transponerLista m l = Prelude.concat (toLista (Data.Matrix.fromLists (shuffle (toLista m) l)))
+
+    shuffle :: [[Char]] -> [Int] -> [[Char]]
+    shuffle m l = reverse' (shuffleAux m l [])
+        where
+            shuffleAux :: [[Char]] -> [Int] -> [[Char]] -> [[Char]]
+            shuffleAux _ [] r     = r
+            shuffleAux m (l:ls) r = shuffleAux m ls ((m !! (l-1)):r)
+    
+    reverse' :: [a] -> [a]
+    reverse' xs = reverseAux xs []
+      where
+        reverseAux :: [a] -> [a] -> [a]
+        reverseAux [] lst     = lst
+        reverseAux (x:xs) lst = reverseAux xs (x:lst)
+
+    addEspacios :: String -> [Bool] -> Int -> String
+    addEspacios xs lst a = addEspaciosAux xs lst a 1
+        where
+            addEspaciosAux :: String -> [Bool] -> Int -> Int -> String
+            addEspaciosAux xs [] _ _ = xs
+            addEspaciosAux xs (l:lst) a tam
+                | l == True = addEspaciosAux (insert (tam*a) ' ' xs) lst a (tam+1)
+                | otherwise = addEspaciosAux xs lst a (tam+1)
+
+    insert :: Int -> Char -> String -> String
+    insert 0 y xs = y:xs
+    insert n y [] = [y]
+    insert n y xs
+        | Prelude.length xs < n = xs
+        | otherwise = (Prelude.take (n-1) xs) Prelude.++ [y] Prelude.++ (Prelude.drop (n-1) xs)
+
+    descifrar :: [String] -> [Int] -> String
+    descifrar texto clave = fase4
+        where
+            fase1   = partirMasResto (unirStrings texto) (Prelude.length clave)
+            fase1_5 = columnasConEspacios (toLista fase1)
+            fase2   = transponerLista fase1 clave
+            fase3   = addEspacios (eliminarEspacios fase2) fase1_5 a
+            fase4   = eliminarEspacios (transponerLista (partirMasResto fase3  (Prelude.length clave)) clave)
+            a = (Prelude.length fase2) `div` (Prelude.length clave)
