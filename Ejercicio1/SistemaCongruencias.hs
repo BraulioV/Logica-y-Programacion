9f9155077f30ad48b75aecf00cb2bd0663f9ada1
BraulioV <brauliovarlop@correo.ugr.es>
2016-02-16 20:31:18 +0100

    sistema congruencias

--- Ejercicio1/SistemaCongruencias.hs
+++ Ejercicio1/SistemaCongruencias.hs
@@ -14,7 +14,19 @@ module SistemaCongruencias (module SistemaCongruencias) where
 
     instance (Integral a) => SistemasConNormalizacion (Sistema a) where
         normSistema (Sistema (Congruencia a1 a2 a3) (Congruencia b1 b2 b3))
-            | d /= 1    = error "Sistema sin solucion"
-            | otherwise = Sistema (norm (Congruencia a1 a2 a3)) (norm (Congruencia b1 b2 b3))
+            | a3 `mod` d /= 0  = error "Sistema sin solucion"
+            | otherwise        = Sistema (norm (Congruencia a1 a2 a3)) (norm (Congruencia b1 b2 b3))
             where 
                 (d,s,_) = xeuclides a3 b3
+
+
+    solve :: (Integral a) => (Sistema a) -> Int -> Int -> (Int, Int)
+    solve sistema lim_inf lim_sup = (li, lsup)
+        where
+            normalizado = normSistema sistema
+            ec1         = ecuacion1 normalizado
+            ec2         = ecuacion2 normalizado
+            k1          = norm (Congruencia (modul ec1) ((b ec2) - (b ec1)) (modul ec2))
+            k           = norm (Congruencia 1 ((b ec2) + ((modul ec1)*(b k1))) ((modul ec1)*(modul k1)))
+            li          = round ((fromIntegral (lim_inf - (a k)))/(fromIntegral(modul k))) --, (fromIntegral(lim_sup - (a k)))/(modul k))
+            lsup        = round ((fromIntegral(lim_sup - (a k)))/(fromIntegral(modul k)))
