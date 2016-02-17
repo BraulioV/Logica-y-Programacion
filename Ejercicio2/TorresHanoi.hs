6ecffd063fe3c1bb39c2629786ef8e9589a84abe
mgmacias95 <mgmacias95@correo.ugr.es>
2016-02-17 09:35:57 +0100

    torres de hanoi completas a falta del menu

--- Ejercicio2/TorresHanoi.hs
+++ Ejercicio2/TorresHanoi.hs
@@ -1,13 +1,28 @@
-module TorresHanoi (module Congruencia) where
+module TorresHanoi (module TorresHanoi) where
+
+    import System.IO
+    import Data.Char
+
+    pasosTorresHanoiAux :: Int -> Char -> Char -> [Char] -> String
+    pasosTorresHanoiAux a s t r = "mover disco " ++ [(r !! (a-1))] ++ " desde " ++ [s] ++ " hasta " ++ [t]
+
+    pasosTorresHanoi' :: Int -> Char -> Char -> Char -> [Char] -> [String]
+    pasosTorresHanoi' 0 _ _ _ _ = []
+    pasosTorresHanoi' a s h t r = pasosTorresHanoi' (a-1) s t h r ++ [pasosTorresHanoiAux a s t r] ++ pasosTorresHanoi' (a-1) h s t r
 
     pasosTorresHanoi :: Int -> IO()
-    pasosTorresHanoi 1 = putStrLn "Mueva el disco superior de Izq a Der"
-    pasosTorresHanoi x
-        | odd x     = putStrLn "Mueva el disco superior de Izq a Der"
-        | otherwise = putStrLn "Mueva el disco superior de Izq a Cen"
+    pasosTorresHanoi a = putStrLn (unlines (pasosTorresHanoi' a 'A' 'B' 'C' discos))
+        where
+            discos = reverse' (map (intToDigit) [1..a])
 
-    pasosTorresHanoi :: (Int, Int, Int) -> IO()
-    pasosTorresHanoi (x,0,0)
-        | odd x     = putStrLn "Mueva el disco superior de Izq a Der"
-        | otherwise = putStrLn "Mueva el disco superior de Izq a Cen"
-    pasosTorresHanoi (x,y,z)n
+    reverse' :: [a] -> [a]
+    reverse' xs = reverseAux xs []
+      where
+        reverseAux :: [a] -> [a] -> [a]
+        reverseAux [] lst     = lst
+        reverseAux (x:xs) lst = reverseAux xs (x:lst)
+
+    pasosTorresHanoi_txt :: Int -> String -> IO()
+    pasosTorresHanoi_txt x nombre = writeFile nombre (unlines (pasosTorresHanoi' x 'A' 'B' 'C' discos))
+        where
+            discos = reverse' (map (intToDigit) [1..x])
