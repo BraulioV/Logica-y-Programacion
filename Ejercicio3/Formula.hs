844d0873da189ebda53cddd47556d0b47f3b11e4
mgmacias95 <mgmacias95@correo.ugr.es>
2016-02-18 23:15:11 +0100

    mostrar completo

--- Ejercicio3/Formula.hs
+++ Ejercicio3/Formula.hs
@@ -1,8 +1,14 @@
 module Formula (module Formula) where
 
     import Arbol
+    import Data.List
 
-    data Formula = ArbolB Char
+    --data Formula a b = (ArbolB a, [b])
+
+    data Formula a b = Formula
+            { arbol :: (ArbolB Char)
+            , listaInts :: ([Int],[Int])
+            }
 
     quitarParentesis :: String -> String
     quitarParentesis xs = quitarParentesis' (filter (/= ')') xs)
@@ -21,12 +27,13 @@ module Formula (module Formula) where
         Caso 2.1: (a v b) -> ¬c
         Caso 2.2: (¬a v b) -> c
     -}
-    crear :: String -> ArbolB Char
+    crear :: String -> Formula a b
     crear xs
-        | (head xs) == '(' = crearCaso2 ps ((length ps) - 1)
-        | otherwise = crearCaso1 ps
+        | (head xs) == '(' = Formula (crearCaso2 ps ((length ps) - 1)) par
+        | otherwise = Formula (crearCaso1 ps) par
             where
-                ps = eliminarEspacios (quitarParentesis xs)
+                ps  = eliminarEspacios (quitarParentesis xs)
+                par = ((elemIndices '(' (eliminarEspacios xs)), (elemIndices ')' (eliminarEspacios xs)))
     
     crearCaso2 :: String -> Int -> ArbolB Char
     crearCaso2 xs 0 = hojaB (head xs)
@@ -42,3 +49,44 @@ module Formula (module Formula) where
     crearCaso1 xs  
         | (xs !! 0) == 'n' = NodoB (NodoB (hojaB (xs !! 1)) (xs !! 0) VacioB) (xs !! 2) (crearCaso1 (drop 3 xs))
         | otherwise        = NodoB (hojaB (xs !! 0)) (xs !! 1) (crearCaso1 (drop 2 xs))
+
+    addPar :: String -> [Int] -> [Int] -> String -> String
+    addPar _ [] [] s         = s
+    addPar y (l:ls) (k:ks) s = addPar w ls ks (s ++ w)
+        where
+            w = (insertar l '(' (insertar k ')' y))
+
+    insertar :: Int -> Char -> String -> String
+    insertar 0 y xs = y:xs
+    insertar n y [] = [y]
+    insertar n y xs
+        | length xs < n = xs
+        | otherwise = (take (n-1) xs) ++ [y] ++ (drop (n-1) xs)
+
+    obtenerForm :: ArbolB Char -> [Char]
+    obtenerForm VacioB = []
+    obtenerForm (NodoB i r d) = (obtenerForm i) ++ [r] ++ obtenerForm d
+
+    toString :: Formula a b -> String
+    toString f = addPar (obtenerForm (arbol f)) (fst (listaInts f)) (snd (listaInts f)) ""
+
+    reverse' :: [a] -> [a]
+    reverse' xs = reverseAux xs []
+      where
+        reverseAux :: [a] -> [a] -> [a]
+        reverseAux [] lst     = lst
+        reverseAux (x:xs) lst = reverseAux xs (x:lst)
+
+
+    instance Show a => Show (Formula a b) where
+        showsPrec _ f = shows (reverse' (mostrar (toString f) ""))
+            where 
+                mostrar :: String -> String -> String
+                mostrar "" s = s
+                mostrar (x:xs) s
+                    | x == 'n'  = mostrar xs ('¬':s)
+                    | x == 'c'  = mostrar xs ('>':'-':s)
+                    | x == 'k'  = mostrar xs ('^':s)
+                    | x == 'o'  = mostrar xs ('v':s)
+                    | x == 'e'  = mostrar xs ('>':'-':'<':s)
+                    | otherwise = mostrar xs (x:s)
