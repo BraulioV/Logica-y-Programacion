a86e9676cd10b61fd3eef5f88f387e6a63b1aea6
mgmacias95 <mgmacias95@correo.ugr.es>
2016-02-18 15:11:55 +0100

    segunda aproximacion: incluye algunos casos de negacion

--- Ejercicio3/Formula.hs
+++ Ejercicio3/Formula.hs
@@ -10,18 +10,30 @@ module Formula (module Formula) where
             quitarParentesis' :: String -> String
             quitarParentesis' xs = filter (/='(') xs
 
+    eliminarEspacios :: String -> String
+    eliminarEspacios xs = filter (/= ' ') xs
+
+    {-
+        Caso 1: a v (b -> c)
+        Caso 2: (a v b) -> c
+        Caso 3: (¬a v b) -> c
+    -}
     crear :: String -> ArbolB Char
     crear xs
         | (head xs) == '(' = crearCaso2 ps ((length ps) - 1)
         | otherwise = crearCaso1 ps
             where
-                ps = quitarParentesis xs
+                ps = eliminarEspacios (quitarParentesis xs)
     
     crearCaso2 :: String -> Int -> ArbolB Char
     crearCaso2 xs 0 = hojaB (head xs)
-    crearCaso2 xs n = NodoB (crearCaso2 xs (n-2)) (xs !! (n-1)) (hojaB (xs !! n))
+    crearCaso2 xs n 
+        | (xs !! (n-1)) == 'n' = NodoB (crearCaso2 xs (n-3)) (xs !! (n-2)) (NodoB (hojaB (xs !! n)) (xs !! (n-1)) VacioB)
+        | otherwise            = NodoB (crearCaso2 xs (n-2)) (xs !! (n-1)) (hojaB (xs !! n))
 
     crearCaso1 :: String -> ArbolB Char
     crearCaso1 []  = VacioB
     crearCaso1 [x] = hojaB x
-    crearCaso1 xs  = NodoB (hojaB (xs !! 0)) (xs !! 1) (crearCaso1 (drop 2 xs))
+    crearCaso1 xs  
+        | (xs !! 0) == 'n' = NodoB (NodoB (hojaB (xs !! 1)) (xs !! 0) VacioB) (xs !! 2) (crearCaso1 (drop 3 xs))
+        | otherwise        = NodoB (hojaB (xs !! 0)) (xs !! 1) (crearCaso1 (drop 2 xs))
