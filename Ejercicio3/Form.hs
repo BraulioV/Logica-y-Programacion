03446efa4a11f1a2d1a54849bc8e1e7db958c5ee
BraulioV <brauliovarlop@correo.ugr.es>
2016-02-21 14:00:24 +0100

    .

--- Ejercicio3/Form.hs
+++ Ejercicio3/Form.hs
@@ -1,5 +1,7 @@
 module Form (module Form) where
 
+    import Data.Maybe (fromMaybe)
+
     data Expr = Variable Char
                 | N Expr        -- negacion
                 | C Expr Expr   -- implicacion
@@ -16,11 +18,22 @@ module Form (module Form) where
         show (O expr1 expr2) = show expr1 ++ 'v':show expr2
         show (E expr1 expr2) = show expr1 ++ '<':'-':'>':show expr2
 
-    leer :: String -> Expr
-    leer (x:xs)
-        | x == '!'  = N (leer xs)
-        | x == '&'  = K (leer xs)
-        | x == '>'  = C (leer xs)
-        | x == '<'  = E (leer xs)
-        | x == '|'  = O (leer xs)
-        | otherwise = leer xs
+    --leer :: String -> Expr
+    --leer (x:xs)
+    --    | x == '!'  = N (leer xs)
+    --    | x == '&'  = K (leer xs)
+    --    | x == '>'  = C (leer xs)
+    --    | x == '<'  = E (leer xs)
+    --    | x == '|'  = O (leer xs)
+    --    | otherwise = leer xs
+
+    interpret :: Expr -> [(Char, Int)] -> Int
+    interpret (Variable v)  vs = fromMaybe 0 (lookup v vs)
+    interpret (N      expr) vs = ((interpret expr vs)+1)`mod`2
+    interpret (K exp1 exp2) vs = (interpret exp1 vs) * (interpret exp2 vs)
+    interpret (O exp1 exp2) vs = ((interpret exp1 vs) * (interpret exp2 vs) 
+                                        + (interpret exp1 vs) + (interpret exp2 vs)) `mod` 2
+    interpret (C exp1 exp2) vs = interpret (O (N exp1) (exp2)) vs
+    interpret (E exp1 exp2) vs 
+        | (interpret exp1 vs) == (interpret exp2 vs) = 1
+        | otherwise                                  = 0
